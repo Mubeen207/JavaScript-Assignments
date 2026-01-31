6130b1aa08ebcc9fbebbdae0743b6b3a073f2e09
mubeen207 <mubeenulislam207@gmail.com>
2026-01-31 12:54:28 +0500

    Task 4.1 – Manual Iteration

---
+++ Section 4 Iterators & for..of/Task 4.1 – Manual Iteration/app.js
@@ -0,0 +1,26 @@
+const fruits = {
+  apple: {
+    color: "red",
+    taste: "sweet",
+    season: "fall",
+  },
+  banana: {
+    color: "yellow",
+    taste: "sweet",
+    season: "year-round",
+  },
+  orange: {
+    color: "orange",
+    taste: "citrus",
+    season: "winter",
+  },
+  mango: {
+    color: "yellow-orange",
+    taste: "sweet and tangy",
+    season: "summer",
+  },
+};
+for (let fruit in fruits){
+    console.log(fruit);
+    
+}
