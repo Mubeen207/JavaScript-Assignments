78ca1275a1272803823252ef7418c597b60ed1f0
mubeen207 <mubeenulislam207@gmail.com>
2026-02-02 00:51:09 +0500

    Task 1.2 – filter + reduce Combination

---
+++ (ES6) Java Script Advanced Concepts Assignment 2/Section 1 Array Advanced Methods (Deep Practice)/Task 1.1 – map vs forEach vs reduce (Behavior Study)/app.js
@@ -0,0 +1,15 @@
+let marks = [45, 72, 88, 30, 90];
+
+let filter = marks.filter((m) => {
+  console.log("filter Mark: ", m);
+  return m >= 50;
+});
+filter.map((num) => {
+  console.log("Greater Then 50 Marks: ", num);
+});
+let total = filter.reduce((sum, mark) => {
+  console.log("reduce sum: ", sum);
+  console.log("reduce mark: ", mark);
+  return sum + mark;
+}, 0);
+console.log("total Result: ", total);
