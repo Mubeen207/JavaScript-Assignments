81a03ee191a95c3a86ed5767d8c3be3ca1007b24
mubeen207 <mubeenulislam207@gmail.com>
2026-02-02 01:12:21 +0500

    Task 1.3 – Custom Array Method Simulation (Hard)

--- (ES6) Java Script Advanced Concepts Assignment 2/Section 1 Array Advanced Methods (Deep Practice)/Task 1.2 – filter + reduce Combination/app.js
+++ (ES6) Java Script Advanced Concepts Assignment 2/Section 1 Array Advanced Methods (Deep Practice)/Task 1.2 – filter + reduce Combination/app.js
@@ -15,23 +15,3 @@
   }, 0);
   console.log("total Result: ", total);
 }
-//-------------------------------------------------------------
-{
-  let numbers = [1, 2, 3, 4, 5];
-
-  function localMap(numArr, callBackFunction) {
-    let result = [];
-    for (let i = 0; i <= numArr.length - 1; i++) {
-      console.log("Local Map Values In For Loop: ", numArr[i]);
-      let mapedValue = callBackFunction(numArr[i], i, numArr);
-      result.push(mapedValue);
-    }
-    return result;
-  }
-
-  let localMapResult = localMap(numbers, (num) => {
-    return num * 2;
-  });
-  console.log(localMapResult);
-}
-//-------------------------------------------------------------
