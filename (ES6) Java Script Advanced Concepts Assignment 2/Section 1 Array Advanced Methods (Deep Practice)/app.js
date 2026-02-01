84bb272d4bdaedefe4e4aa60cf8c6c47c2e6ca3a
mubeen207 <mubeenulislam207@gmail.com>
2026-02-02 00:40:17 +0500

    Task 1.1 – map vs forEach vs reduce (Behavior Study)

--- (ES6) Java Script Advanced Concepts Assignment 2/Section 1 Array Advanced Methods (Deep Practice)/app.js
+++ (ES6) Java Script Advanced Concepts Assignment 2/Section 1 Array Advanced Methods (Deep Practice)/app.js
@@ -1,9 +1,36 @@
 let numbers = [1, 2, 3, 4];
+{
+  let mapMethode = numbers.map((num) => {
+    console.log("map numbers: ", num);
+    let result = num * 2;
+    console.log("Multiplaying Map Numbers: ", result);
+    return result;
+  });
+  console.log("Map Final: ", mapMethode);
+}
+//---------------------------------------------------------------
 
-let mapMethode = numbers.map((num) => {
-  console.log("map numbers: ", num);
-  let result = num * 2;
-  console.log("Multiplaying Map Numbers: ", result);
-  return result;
-});
-console.log("Map Final: ", mapMethode);
+{
+  let forEatchResult = [];
+
+  numbers.forEach((num) => {
+    console.log("forEach numbers: ", num);
+    let result = num * 2;
+    console.log("Multiplaying forEach Numbers: ", result);
+    return forEatchResult.push(result);
+  });
+  console.log("forEEach Final: ", forEatchResult);
+}
+//---------------------------------------------------------------
+
+{
+  let reduceResult = numbers.reduce((acc, num) => {
+    console.log("Reduce Pre Total: ", acc);
+    console.log("Reduce num: ", num);
+    let result = num * 2;
+    acc.push(result);
+    console.log("reduce outPut: ", acc);
+    return acc;
+  }, []);
+  console.log("reuce Final: ", reduceResult);
+}
